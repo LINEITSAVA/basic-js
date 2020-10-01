28a07ceed720a0580757fb9ee5209ef2ca0c5c93
Yuliya Lineitsava <serata@mail.ru>
2020-10-01 19:35:47 +0300

    feat: add some solutions to the tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,24 +2,19 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
     getLength() {
-    chainMaker = chainMaker.split("~~").length;
-    return chainMaker;
+
   },
   addLink(value) {
-    chainMaker = chainMaker.split("~~").push(value);
-    return chainMaker;
+
   },
   removeLink(position) {
-    chainMaker = chainMaker.split("~~").splice(position,1);
-    return chainMaker;
+
   },
   reverseChain() {
-    chainMaker = chainMaker.split("~~").reverse();
-    return chainMaker;
+
   },
   finishChain() {
-    chainMaker = chainMaker.split("~~").toString();
-    return chainMaker;
+    
   }
 };
 
